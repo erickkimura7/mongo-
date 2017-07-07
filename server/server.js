75f0c5149b2ad784ea9effcc26d9955ed1b94dbd
erick_kimura <erickkimura7@gmail.com>
2017-07-07 19:55:18 +0000

    save

--- server/server.js
+++ server/server.js
@@ -12,27 +12,27 @@ var app = express();
 
 app.use(bodyParser.json());
 
-// app.post('/todos',(req,res)=>{
-//     var todo = new Todo({
-//       text: req.body.text,
-//       completed: req.body.completed
-//     });
-//     todo.save().then((doc)=> {
-//         res.send(doc);
+app.post('/todos',(req,res)=>{
+    var todo = new Todo({
+      text: req.body.text,
+      completed: req.body.completed
+    });
+    todo.save().then((doc)=> {
+        res.send(doc);
         
-//     }, (e) => {
-//         res.status(400).send(e);
-//     });
-//     console.log(req.body);    
-// });
+    }, (e) => {
+        res.status(400).send(e);
+    });
+    console.log(req.body);    
+});
 
-// app.get('/todos',(req,res)=>{
-//   Todo.find().then((todos)=>{
-//       res.send({todos});
-//   }, (e) => {
-//       res.status(400).send(e);
-//   }); 
-// });
+app.get('/todos',(req,res)=>{
+  Todo.find().then((todos)=>{
+      res.send({todos});
+  }, (e) => {
+      res.status(400).send(e);
+  }); 
+});
 
 app.get('/todos/:id',(req,res) => {
    var id = req.params.id;
